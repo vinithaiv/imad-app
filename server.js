9473c262c49a4ab90f4668daecdde53e4ae4ca8a
Vinitha I V <vinithaiv@gmail.com>
2017-08-29 00:50:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,14 +109,9 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/article-one', function (req, res) {
-    res.send(createTemplate(articleOne));
-});
-app.get('/article-two',function(req, res) {
-    res.sendFile((path.join(__dirname, 'ui', 'article-two.html')));
-});
-app.get('/article-three', function(req, res){
-    res.sendFile((path.join(__dirname, 'ui', 'article-three.html')));
+app.get('/:articleName', function (req, res) {
+    var articleName=req.parames.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
