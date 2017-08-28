b0d1b86b80f5ddcbc63fc425e9b9edd68a725120
Vinitha I V <vinithaiv@gmail.com>
2017-08-28 23:17:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+
 
 var articleOne = { 
     title: "Article One| Vinitha I V",
@@ -27,13 +25,54 @@ var articleOne = {
            
 };
 
-
-var htmlTemplate{
+function createTemplate(data){
     
+  var title= data.title;
+  var date= data.date;
+  var heading= data.heading;
+  var content= data.content;
+
+var htmlTemplate =
+    <html>
+    <head>
+        <title>
+           ${title}
+        </title>
+        <link href="ui/style.css" rel="stylesheet"/>
+    </head>
+    
+    
+    <body>
+    <div class="container">
+       <div>
+           <a href="/">Home</a>
+       </div>
+       <h3>
+          ${heading}
+       </h3>
+       <div>
+           ${date}
+       </div>
+       <div>
+           ${content}
+        </div>   
+    </div>
+    
+    </body>
+    </html>
+;
+    return htmlTemplate;
 }
     
+    
+    
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
 app.get('/article-one', function (req, res) {
-    res.sendFile((path.join(__dirname, 'ui', 'article-one.html')));
+    res.send(createTenplate(articleOne));
 });
 app.get('/article-two',function(req, res) {
     res.sendFile((path.join(__dirname, 'ui', 'article-two.html')));
