f738e9325b824aef5556660feb8fd5dc00a235a2
Vinitha I V <vinithaiv@gmail.com>
2017-08-30 01:11:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ element.innerHTML="Changed maintext";
      
  var marginLeft=0; 
  function moveRight() {
-     marginLeft=marginLeft+5;
+     marginLeft=marginLeft+1;
      img.style.marginLeft=marginLeft+"px";
      
  }
