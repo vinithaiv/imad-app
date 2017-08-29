6b7edbd0afdb79ff26ca6b36c4f1e4f08435ee16
Vinitha I V <vinithaiv@gmail.com>
2017-08-30 01:10:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,14 @@ element.innerHTML="Changed maintext";
   
  //Move element
  var img=document.getElementById('madi');
+     
+ var marginLeft=0; 
+ function moveRight() {
+     marginLeft=marginLeft+10;
+     img.style.marginLeft=marginLeft+"px";
+     
+ }
+ 
  img.onclick = function() {
-     img.style.marginLeft= "100px";
+     var interval=setInterval(moveRight,100);
  };
