2186c44b52b4f045d9ac0df85bc4e3f2c5fea572
Vinitha I V <vinithaiv@gmail.com>
2017-08-31 00:02:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,6 +108,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    re.send(counter.toString());
+});
 
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
