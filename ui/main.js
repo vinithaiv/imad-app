3ff41b1acd310f1556664f9f3d7f0b915505bdf6
Vinitha I V <vinithaiv@gmail.com>
2017-08-31 01:48:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -59,4 +59,13 @@ element.innerHTML="Changed maintext";
      
      
      //capture a list of names and render it as list
- }
+    var names = ['name1', 'name2' , 'name3', 'name4'];
+    var list = '';
+    for(var i=0; i<names.length, i++)
+        list == '<li>' + names[i] + </li>;
+        
+    var ul=document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+ };
+ 
