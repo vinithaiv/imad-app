e7b0fe9986365c57c022e2b63db30045e6f0befc
Vinitha I V <vinithaiv@gmail.com>
2017-08-31 01:14:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ element.innerHTML="Changed maintext";
  button.onclick=function () {
      counter = counter+1;
      var span=document.getElementById('count');
-     span.innerHTML=counter.toString;
+     span.innerHTML=counter.toString();
      
      //Create a request object
     /* var request=XMLHttpRequest();
