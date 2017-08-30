c75288b55051af9649059fcb609519b45e834a27
Vinitha I V <vinithaiv@gmail.com>
2017-08-31 02:15:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,6 +114,18 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
+
+ var names=[];
+ app.get('/submit-name/:name',function(req,res) { //URL: /submit-name?name-xxxx
+     //get name from request
+     
+     var name = req.query.name;
+     //JSON Javascript object notification
+     names.push(name);
+     res.send(JSON.stringify(names));
+ });
+ 
+
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
@@ -132,16 +144,6 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
- var names=[];
- app.get('/submit-name/:name',function(req,res) { //URL: /submit-name?name-xxxx
-     //get name from request
-     
-     var name = req.query.name;
-     //JSON Javascript object notification
-     names.push(name);
-     res.send(JSON.stringify(names));
- });
- 
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
