6fe93d092fac07851bfd297a989696e62476f0a1
Vinitha I V <vinithaiv@gmail.com>
2017-08-31 01:59:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -55,14 +55,14 @@ element.innerHTML="Changed maintext";
  var nameInput = document.getElementById('name');
  var name = nameInput.value;
  var submit = document.getElementById('submit_btn');
- submit.onclick = function(){
+ submit.onclick = function () {
      
      
      //capture a list of names and render it as list
     var names = ['name1', 'name2' , 'name3', 'name4'];
     var list = '';
-    for(var i=0; i<names.length; i++)
-        list == '<li>' + names[i] + '</li>';
+    for(var i=0; i < names.length; i++)
+        {list == '<li>' + names[i] + '</li>';}
         
     var ul=document.getElementById('namelist');
     ul.innerHTML = list;
